6d60050bba35392dafe0fe8a35f311496cfa157b
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2019-06-29 16:56:36 +0800

    Implementation func shorten cuts and concatenates a part of string with ellipsis if its length long than 500 bytes.

---
+++ string/shorten.go
@@ -0,0 +1,14 @@
+package str
+
+// Shorten it cuts and concatenates a part of string with ellipsis if its length long than 500 bytes.
+func Shorten(b []byte) string {
+	var bodyChunk string
+	maxBodyChunk := 500
+	bodyStr := string(b)
+	if len(bodyStr) > maxBodyChunk {
+		bodyChunk = bodyStr[:maxBodyChunk] + "..."
+	} else {
+		bodyChunk = bodyStr
+	}
+	return bodyChunk
+}
