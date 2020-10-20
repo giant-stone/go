d0298f43fcef30c2c26743354e7aa34dadb7fc90
tony95271 <63030915+tony95271@users.noreply.github.com>
2020-10-20 21:34:08 +0800

    bugfix

--- gsql/get_test.go
+++ gsql/get_test.go
@@ -16,10 +16,13 @@ func TestGSql_GetsWhere(t *testing.T) {
 	setUp(db)
 
 	// insert samples
+	obj1 := account{Id: 1, Mobileno: "12345", Password: "12345"}
+	obj2 := account{Id: 2, Mobileno: "10000", Password: "10000"}
+	obj3 := account{Id: 3, Mobileno: "9999", Password: "9999"}
 	changes := []interface{}{
-		account{Id: 1, Mobileno: "12345", Password: "12345"},
-		account{Id: 2, Mobileno: "10000", Password: "10000"},
-		account{Id: 3, Mobileno: "9999", Password: "9999"},
+		obj1,
+		obj2,
+		obj3,
 	}
 	_, err = mgr.BulkCreateOrUpdate(db, changes)
 	gutil.ExitOnErr(err)
@@ -53,6 +56,25 @@ func TestGSql_GetsWhere(t *testing.T) {
 		}
 
 	}
+
+	// query samples
+	objsGot = []account{}
+	where := []map[string]interface{}{
+		map[string]interface{}{"key": "id", "op": "=", "value": 2},
+	}
+	err = mgr.GetsWhere(db, &objsGot, &columns, &where, limit)
+	if err != nil {
+		t.Errorf("want GetsWhere err=nil, got %v", err)
+	}
+	wantTotal := 1
+	if wantTotal != len(objsGot) {
+		t.Errorf("want len(objsGot)=%d, got %d", wantTotal, len(objsGot))
+	}
+
+	objGot := objsGot[0]
+	gutil.CmpExpectedGot(t, "id", obj2.Id, objGot.Id)
+	gutil.CmpExpectedGot(t, "mobileno", obj2.Mobileno, objGot.Mobileno)
+	gutil.CmpExpectedGot(t, "password", obj2.Password, objGot.Password)
 
 	tearDown(db)
 }
