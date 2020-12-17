5c985e53849a9a71e54b8dcb076086b54ec65662
tony95271 <63030915+tony95271@users.noreply.github.com>
2020-12-17 15:59:53 +0800

    add ghttp post examples

--- ghttp/example_test.go
+++ ghttp/example_test.go
@@ -1,9 +1,12 @@
 package ghttp_test
 
 import (
+	"bytes"
 	"fmt"
 	"log"
+	"mime/multipart"
 	"net/url"
+	"os"
 	"time"
 
 	"github.com/giant-stone/go/ghttp"
@@ -24,13 +27,13 @@ func ExampleNew() {
 	ghttp.CheckRequestErr(fullurl, req.RespStatus, req.RespBody, err)
 }
 
-
-func ExamplePostInUrlencoded() {
+// ExampleHttpRequest_SetPostBody show howto POST in application/x-www-form-urlencoded
+func ExampleHttpRequest_SetPostBody() {
 	rq := ghttp.New().
 		SetDebug(true).
 		SetRequestMethod("POST").
 		SetUri("https://httpbin.org/post").
-		SetTimeout(time.Second*5)
+		SetTimeout(time.Second * 5)
 
 	form := url.Values{}
 	form.Add("id", fmt.Sprintf("%d", 123))
@@ -42,6 +45,44 @@ func ExamplePostInUrlencoded() {
 	rq.SetPostBody(&rqBody)
 	err := rq.Send()
 	gutil.CheckErr(err)
+
+	log.Println(
+		rq.RespStatus,
+		string(rq.RespBody),
+	)
+}
+
+// ExampleHttpRequest_SetPostBody2 show howto POST in multipart/form-data
+func ExampleHttpRequest_SetPostBody2() {
+	var err error
+
+	rq := ghttp.New().
+		SetDebug(true).
+		SetRequestMethod("POST").
+		SetUri("https://httpbin.org/post").
+		SetTimeout(time.Second * 5)
+
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+
+	err = ghttp.AppendMultipartFormData(w, "myfile", "myfile.data", []byte(`hello 中文`))
+	gutil.ExitOnErr(err)
+
+	err = ghttp.AppendMultipartFormData(w, "myfile2", "myfile2.data", []byte(`foo\nbar`))
+	gutil.ExitOnErr(err)
+
+	err = w.WriteField("id", "123")
+	gutil.ExitOnErr(err)
+
+	err = w.Close()
+	gutil.ExitOnErr(err)
+
+	rqBody := b.Bytes()
+
+	rq.SetPostBody(&rqBody)
+	rq.SetHeader("Content-Type", w.FormDataContentType())
+	err = rq.Send()
+	gutil.ExitOnErr(err)
 
 	log.Println(
 		rq.RespStatus,
