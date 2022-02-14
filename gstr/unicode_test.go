70b1668558c6c12f33504cb38b3b35b58a20c131
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-02-14 21:49:22 +0800

    add Aoti, Aoti64 and IsInteger shortcut functions

--- gstr/unicode_test.go
+++ gstr/unicode_test.go
@@ -3,20 +3,20 @@ package gstr_test
 import (
 	"log"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/giant-stone/go/gstr"
-	"github.com/giant-stone/go/gutil"
 )
 
 var (
 	samplesIsInteger = []struct {
-		S        string
-		Expected bool
+		s    string
+		want bool
 	}{
 		{"123", true},
 		{"a", false},
-		{" 123 ", true},
+		{" 123 ", false},
 		{"a 123", false},
 		{"中文", false},
 		{"123中文", false},
@@ -24,16 +24,19 @@ var (
 )
 
 func TestIsInteger(t *testing.T) {
-	for _, sample := range samplesIsInteger {
-		gutil.CmpExpectedGot(t, sample.S, sample.Expected, gstr.IsInteger(sample.S))
+	for _, item := range samplesIsInteger {
+		got := gstr.IsInteger(item.s)
+		if !reflect.DeepEqual(item.want, got) {
+			t.Errorf("IsInteger -%v- want %v got %v", item.s, item.want, got)
+		}
 	}
 
 }
 
 var (
 	samplesIsAscii = []struct {
-		S        rune
-		Expected bool
+		s    rune
+		want bool
 	}{
 		{'1', true},
 		{'a', true},
@@ -44,15 +47,18 @@ var (
 )
 
 func TestIsAscii(t *testing.T) {
-	for _, sample := range samplesIsAscii {
-		gutil.CmpExpectedGot(t, sample.S, sample.Expected, gstr.IsAscii(sample.S))
+	for _, item := range samplesIsAscii {
+		got := gstr.IsAscii(item.s)
+		if !reflect.DeepEqual(item.want, got) {
+			t.Errorf("IsAscii -%v- want %v got %v", item.s, item.want, got)
+		}
 	}
 }
 
 var (
 	samplesIsAlphabet = []struct {
-		S        rune
-		Expected bool
+		s    rune
+		want bool
 	}{
 		{'1', false},
 		{'a', true},
@@ -63,8 +69,11 @@ var (
 )
 
 func TestIsAlphabet(t *testing.T) {
-	for _, sample := range samplesIsAlphabet {
-		gutil.CmpExpectedGot(t, sample.S, sample.Expected, gstr.IsAlphabet(sample.S))
+	for _, item := range samplesIsAlphabet {
+		got := gstr.IsAlphabet(item.s)
+		if !reflect.DeepEqual(item.want, got) {
+			t.Errorf("IsAlphabet -%v- want %v got %v", item.s, item.want, got)
+		}
 	}
 }
 
