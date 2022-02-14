caeee78d861952b801df451a33945eda38545848
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-02-14 21:48:37 +0800

    fix Shorten func

--- gstr/shorten.go
+++ gstr/shorten.go
@@ -2,20 +2,8 @@ package gstr
 
 import (
 	"strings"
+	"unicode/utf8"
 )
-
-// Shorten it cuts and concatenates a part of string with ellipsis if its length long than maxLen in unicode.
-func Shorten(b []byte, maxLen int) string {
-	var bodyChunk []rune
-	body := []rune(string(b))
-	if len(body) > maxLen {
-		bodyChunk = body[:maxLen]
-		bodyChunk = append(bodyChunk, []rune("...")...)
-	} else {
-		bodyChunk = body
-	}
-	return string(bodyChunk)
-}
 
 // TrimSubstrings remove all needles item from haystack.
 func TrimSubstrings(haystack string, needles []string) (result string) {
@@ -23,4 +11,51 @@ func TrimSubstrings(haystack string, needles []string) (result string) {
 		haystack = strings.ReplaceAll(haystack, item, "")
 	}
 	return haystack
+}
+
+const (
+	DefaultShortenSuffix = "…"
+)
+
+// Shorten it cuts and concatenates a part of string with ellipsis if its length long than maxLen in unicode.
+func Shorten(s string, n int) (rs string) {
+	strLen := utf8.RuneCountInString(s)
+
+	if strLen <= n {
+		rs = s
+	} else {
+		rs = Substring(s, 0, n)
+	}
+	return
+}
+
+func LenUtf8(s string) (n int) {
+	return utf8.RuneCountInString(s)
+}
+
+func ShortenWith(s string, n int, suffix string) (rs string) {
+	strLen := utf8.RuneCountInString(s)
+
+	if strLen <= n {
+		rs = s
+	} else {
+		rs = Substring(s, 0, n-1) + suffix
+	}
+	return
+}
+
+// https://stackoverflow.com/questions/28718682/how-to-get-a-substring-from-a-string-of-runes-in-golang
+func Substring(s string, start int, end int) string {
+	start_str_idx := 0
+	i := 0
+	for j := range s {
+		if i == start {
+			start_str_idx = j
+		}
+		if i == end {
+			return s[start_str_idx:j]
+		}
+		i++
+	}
+	return s[start_str_idx:]
 }
