c56189dfc3dfb8b27c614e936a1d2d594d820da5
tony95271 <63030915+tony95271@users.noreply.github.com>
2022-02-14 21:48:12 +0800

    add Sha1 shortcut func

--- gstr/hash.go
+++ gstr/hash.go
@@ -2,6 +2,7 @@ package gstr
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
 	"encoding/hex"
 )
 
@@ -9,5 +10,12 @@ import (
 func Md5(text string) string {
 	algorithm := md5.New()
 	algorithm.Write([]byte(text))
+	return hex.EncodeToString(algorithm.Sum(nil))
+}
+
+// Sha1 hashes a string in sha1 algorithm.
+func Sha1(s string) (rs string) {
+	algorithm := sha1.New()
+	algorithm.Write([]byte(s))
 	return hex.EncodeToString(algorithm.Sum(nil))
 }
