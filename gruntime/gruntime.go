4cb214c790b9974303587087a7f6dc91ac7a7796
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-05-06 23:17:21 +0800

    rename package gref to gruntime

--- gruntime/gruntime.go
+++ gruntime/gruntime.go
@@ -1,4 +1,4 @@
-package gref
+package gruntime
 
 import (
 	"reflect"
@@ -9,5 +9,4 @@ import (
 // https://stackoverflow.com/questions/7052693/how-to-get-the-name-of-a-function-in-go
 func GetFunctionName(i interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
-
 }
