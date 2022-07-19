0ece6194a55adb58bd16e52e3ecc435253c64804
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-07-19 22:20:38 +0800

    add Sha1FromBytes and Md5FromBytes

--- gstr/hash.go
+++ gstr/hash.go
@@ -6,10 +6,17 @@ import (
 	"encoding/hex"
 )
 
-// Md5 hashes using md5 algorithm
+// Md5 hashes using md5 algorithm.
 func Md5(text string) string {
 	algorithm := md5.New()
 	algorithm.Write([]byte(text))
+	return hex.EncodeToString(algorithm.Sum(nil))
+}
+
+// Md5FromBytes hashes a slice of bytes in md5 algorithm.
+func Md5FromBytes(data []byte) string {
+	algorithm := md5.New()
+	algorithm.Write(data)
 	return hex.EncodeToString(algorithm.Sum(nil))
 }
 
@@ -17,5 +24,12 @@ func Md5(text string) string {
 func Sha1(s string) (rs string) {
 	algorithm := sha1.New()
 	algorithm.Write([]byte(s))
+	return hex.EncodeToString(algorithm.Sum(nil))
+}
+
+// Sha1FromBytes hashes a slice of bytes in sha1 algorithm.
+func Sha1FromBytes(data []byte) (rs string) {
+	algorithm := sha1.New()
+	algorithm.Write(data)
 	return hex.EncodeToString(algorithm.Sum(nil))
 }
