1d64e91b8f797859d709531eb21cc8b0a7fe459d
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-09-15 02:01:06 +0800

    ghttp supports gomock (#14)
    * rename var its to it
    
    * refactor ghttp for mock
    
    * Update ghttp_test.go

--- ghttp/req.go
+++ ghttp/req.go
@@ -37,18 +37,21 @@ type HttpRequest struct {
 	Proxy string
 
 	// request send at timestamp in unix(milliseconds)
-	Rqts int64
+	Rqts    int64
+	Elapsed time.Duration
 
+	// DEPRECATED.
 	RespStatus int
-	RespBody   []byte
+	// DEPRECATED.
+	RespBody []byte
+	// DEPRECATED.
 	RespHeader http.Header
-	Elapsed    time.Duration
 }
 
 func New() *HttpRequest {
 	return &HttpRequest{
 		Ctx:     context.Background(),
-		Method:  defaultMethod,
+		Method:  "",
 		Headers: map[string]interface{}{},
 		Timeout: time.Duration(10) * time.Second,
 	}
@@ -57,76 +60,78 @@ func New() *HttpRequest {
 func NewWithCtx(ctx context.Context) *HttpRequest {
 	return &HttpRequest{
 		Ctx:     ctx,
-		Method:  defaultMethod,
+		Method:  "",
 		Headers: map[string]interface{}{},
 		Timeout: time.Duration(10) * time.Second,
 	}
 }
 
-func (its *HttpRequest) SetRandomUserAgent(flag bool) *HttpRequest {
-	its.UseRandomUserAgent = flag
-	return its
+func (it *HttpRequest) SetRandomUserAgent(flag bool) *HttpRequest {
+	it.UseRandomUserAgent = flag
+	return it
 }
 
-func (its *HttpRequest) SetTimeout(duration time.Duration) *HttpRequest {
-	its.Timeout = duration
-	return its
+func (it *HttpRequest) SetTimeout(duration time.Duration) *HttpRequest {
+	it.Timeout = duration
+	return it
 }
 
-func (its *HttpRequest) SetRequestMethod(method string) *HttpRequest {
-	its.Method = method
-	return its
+func (it *HttpRequest) SetRequestMethod(method string) *HttpRequest {
+	it.Method = method
+	return it
 }
 
-func (its *HttpRequest) SetUri(uri string) *HttpRequest {
-	its.Uri = uri
+func (it *HttpRequest) SetUri(uri string) *HttpRequest {
+	it.Uri = uri
 
-	return its
+	return it
 }
 
-func (its *HttpRequest) SetProxy(addr string) *HttpRequest {
-	its.Proxy = addr
-	return its
+func (it *HttpRequest) SetProxy(addr string) *HttpRequest {
+	it.Proxy = addr
+	return it
 }
 
-func (its *HttpRequest) SetPostBody(body *[]byte) *HttpRequest {
+func (it *HttpRequest) SetPostBody(body *[]byte) *HttpRequest {
 	if body != nil && len(*body) > 0 {
-		its.Body = make([]byte, len(*body))
-		copy(its.Body, *body)
+		it.Body = make([]byte, len(*body))
+		copy(it.Body, *body)
 	}
-	return its
+	return it
 }
 
-func (its *HttpRequest) SetHttpAuth(username, password string) *HttpRequest {
+func (it *HttpRequest) SetHttpAuth(username, password string) *HttpRequest {
 	plain := fmt.Sprintf("%s:%s", username, password)
 	value := "Basic " + base64.StdEncoding.EncodeToString([]byte(plain))
-	its.SetHeader("Authorization", value)
-	return its
+	it.SetHeader("Authorization", value)
+	return it
 }
 
-func (its *HttpRequest) SetHeader(key string, value interface{}) *HttpRequest {
-	its.Headers[key] = value
-	return its
+func (it *HttpRequest) SetHeader(key string, value interface{}) *HttpRequest {
+	it.Headers[key] = value
+	return it
 }
 
-func (its *HttpRequest) SetHeaders(headers map[string]interface{}) *HttpRequest {
+func (it *HttpRequest) SetHeaders(headers map[string]interface{}) *HttpRequest {
 	for key, value := range headers {
-		its.Headers[key] = value
+		it.Headers[key] = value
 	}
-	return its
+	return it
 }
 
-func (its *HttpRequest) Send() (err error) {
+// DEPRECATED.
+//   `Send()` does not supports gomock, use `Do()` instead.
+func (it *HttpRequest) Send() (err error) {
 	client := &http.Client{
-		Timeout: its.Timeout,
+		Timeout: it.Timeout,
 	}
 
 	tr := http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 
-	if its.Proxy != "" {
-		proxyNode := its.Proxy
+	if it.Proxy != "" {
+		proxyNode := it.Proxy
 		if !strings.HasPrefix(proxyNode, "http") {
 			proxyNode = fmt.Sprintf("http://%s", proxyNode)
 		}
@@ -143,45 +148,126 @@ func (its *HttpRequest) Send() (err error) {
 	client.Transport = &tr
 
 	var reqBody io.Reader
-	if len(its.Body) > 0 {
-		reqBody = bytes.NewBuffer(its.Body)
+	if len(it.Body) > 0 {
+		reqBody = bytes.NewBuffer(it.Body)
 	}
 
-	req, err := http.NewRequestWithContext(its.Ctx, its.Method, its.Uri, reqBody)
+	if it.Method == "" {
+		it.Method = defaultMethod
+	}
+
+	req, err := http.NewRequestWithContext(it.Ctx, it.Method, it.Uri, reqBody)
 	if err != nil {
 		return
 	}
 
-	for k, v := range its.Headers {
+	for k, v := range it.Headers {
 		req.Header.Set(k, fmt.Sprintf("%v", v))
 	}
 
 	if req.Header.Get("User-Agent") == "" {
-		if its.UseRandomUserAgent {
+		if it.UseRandomUserAgent {
 			req.Header.Set("User-Agent", gutil.RandChoice(UserAgents).(string))
 		}
 	}
 
 	now := time.Now()
-	its.Rqts = now.UnixNano() / 1000000
+	it.Rqts = now.UnixNano() / 1000000
 	resp, err := client.Do(req)
 	elapsed := time.Since(now)
 
-	glogging.Sugared.Infof("%s %s elapsed=%v err=%v", its.Method, its.Uri, ghuman.FmtDuration(elapsed), err)
+	glogging.Sugared.Infof("%s %s elapsed=%v err=%v", it.Method, it.Uri, ghuman.FmtDuration(elapsed), err)
 
 	if err != nil {
-		its.Elapsed = elapsed
+		it.Elapsed = elapsed
 		return
 	}
 	defer resp.Body.Close()
 
-	its.RespStatus = resp.StatusCode
-	its.RespHeader = resp.Header
+	it.RespStatus = resp.StatusCode
+	it.RespHeader = resp.Header
 
 	RespBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return
 	}
-	its.RespBody = RespBody
+	it.RespBody = RespBody
 	return
+}
+
+func (it *HttpRequest) GenerateRequest() (rs *http.Request) {
+	var rqBody io.Reader
+	if len(it.Body) > 0 {
+		rqBody = io.NopCloser(bytes.NewReader(it.Body))
+	}
+
+	rs, _ = http.NewRequestWithContext(it.Ctx, it.Method, it.Uri, rqBody)
+
+	for k, v := range it.Headers {
+		value := fmt.Sprintf("%v", v)
+		rs.Header.Set(k, value)
+		glogging.Sugared.Debugf("Header.Set %s=%s", k, value)
+	}
+
+	if rs.Header.Get("User-Agent") == "" {
+		if it.UseRandomUserAgent {
+			value := gutil.RandChoice(UserAgents).(string)
+			rs.Header.Set("User-Agent", value)
+
+			glogging.Sugared.Debugf("Header.Set User-Agent=%s", value)
+		}
+	}
+
+	return
+}
+
+// Do implements HttpClient
+func (it *HttpRequest) Do(rq *http.Request) (rs *http.Response, err error) {
+	if Client == nil {
+		client := &http.Client{
+			Timeout: it.Timeout,
+		}
+
+		tr := http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+
+		if it.Proxy != "" {
+			proxyNode := it.Proxy
+			if !strings.HasPrefix(proxyNode, "http") {
+				proxyNode = fmt.Sprintf("http://%s", proxyNode)
+			}
+			glogging.Sugared.Debugf("proxy=%s", proxyNode)
+
+			u, errUrl := url.Parse(proxyNode)
+			if errUrl != nil {
+				err = errUrl
+				return
+			}
+			tr.Proxy = http.ProxyURL(u)
+			client.Transport = &tr
+		}
+		client.Transport = &tr
+
+		Client = client
+	}
+
+	now := time.Now()
+	it.Rqts = now.UnixNano() / 1000000
+	rs, err = Client.Do(rq)
+	elapsed := time.Since(now)
+
+	glogging.Sugared.Debugf("%s %s elapsed=%v err=%v", it.Method, it.Uri, ghuman.FmtDuration(elapsed), err)
+
+	if err != nil {
+		it.Elapsed = elapsed
+		return
+	}
+
+	return
+}
+
+func ReadBody(httpRs *http.Response) (rs []byte, err error) {
+	defer httpRs.Body.Close()
+	return ioutil.ReadAll(httpRs.Body)
 }
