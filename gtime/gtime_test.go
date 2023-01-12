076d4642e68755e3cc373e28e2c1c39eb324cca7
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2023-01-12 19:58:20 +0800

    add UnixTime2YyyymmddLocal and UnixTime2YYYYMMDDHHmmUtc func

--- gtime/gtime_test.go
+++ gtime/gtime_test.go
@@ -51,6 +51,7 @@ func TestUnixTime2YyyymmddUtc(t *testing.T) {
 			want string
 		}{
 			{933984000, "1999-08-07"},
+			{1673478000, "2023-01-11"},
 		}
 	)
 
@@ -59,6 +60,63 @@ func TestUnixTime2YyyymmddUtc(t *testing.T) {
 
 		if !reflect.DeepEqual(item.want, got) {
 			t.Errorf("UnixTime2YyyymmddUtc -%v- want %v got %v", item.s, item.want, got)
+		}
+	}
+}
+
+func TestUnixTime2YyyymmddLocal(t *testing.T) {
+	var (
+		samples = []struct {
+			s    int64
+			want string
+		}{
+			{1673478000, "2023-01-12"},
+		}
+	)
+
+	for _, item := range samples {
+		got := gtime.UnixTime2YyyymmddLocal(item.s)
+
+		if !reflect.DeepEqual(item.want, got) {
+			t.Errorf("UnixTime2YyyymmddLocal -%v- want %v got %v", item.s, item.want, got)
+		}
+	}
+}
+
+func TestUnixTime2YYYYMMDDHHmmUtc(t *testing.T) {
+	var (
+		samples = []struct {
+			s    int64
+			want string
+		}{
+			{1673478001, "2023-01-11 23:00:01"},
+		}
+	)
+
+	for _, item := range samples {
+		got := gtime.UnixTime2YYYYMMDDHHmmUtc(item.s)
+
+		if !reflect.DeepEqual(item.want, got) {
+			t.Errorf("UnixTime2YYYYMMDDHHmmUtc -%v- want %v got %v", item.s, item.want, got)
+		}
+	}
+}
+
+func TestUnixTime2YYYYMMDDHHmmLocal(t *testing.T) {
+	var (
+		samples = []struct {
+			s    int64
+			want string
+		}{
+			{1673478001, "2023-01-12 07:00:01"},
+		}
+	)
+
+	for _, item := range samples {
+		got := gtime.UnixTime2YYYYMMDDHHmmLocal(item.s)
+
+		if !reflect.DeepEqual(item.want, got) {
+			t.Errorf("UnixTime2YYYYMMDDHHmmUtc -%v- want %v got %v", item.s, item.want, got)
 		}
 	}
 }
