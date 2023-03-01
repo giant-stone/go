cec51aae387c331adfae365ab00b7da3c57eb06a
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2023-03-01 13:28:08 +0800

    add func gstrconv.AtoiCn (#23)
    * add func gstrconv.AtoiCn
    
    * refactor strconv and unicode package related functions
    
    * Create atoi.go
    
    * fix run gtime package tests fail on UTC timezone machine

--- gstr/atoi.go
+++ gstr/atoi.go
@@ -1,50 +1,36 @@
+// DEPRECATED. use gstrconv package instead
 package gstr
 
 import (
 	"regexp"
 	"strconv"
-	"unicode"
 )
 
+// DEPRECATED. use gstrconv package instead
 // Atoi parses a string into int.
 func Atoi(s string) (i int) {
 	v, _ := strconv.Atoi(s)
 	return v
 }
 
+// DEPRECATED. use gstrconv package instead
 // Atoi64 parses a string into int64.
 func Atoi64(s string) (rs int64) {
 	i, _ := strconv.ParseUint(s, 10, 64)
 	return int64(i)
 }
 
-// IsInteger check a string if is an integer.
-func IsInteger(s string) (rs bool) {
-	rs = true
-
-	if s == "" {
-		rs = false
-		return
-	}
-
-	for _, r := range s {
-		if !unicode.IsDigit(rune(r)) {
-			rs = false
-			break
-		}
-	}
-	return
-}
-
 var (
 	reDigit = regexp.MustCompile(`(?P<digit>\d+)`)
 )
 
+// DEPRECATED. use gstrconv package instead
 // ParseDigitFromMixed parses a digit from a string contains 0-9 and non 0-9 chars.
 func ParseDigitFromMixed(s string) (i int) {
 	return Atoi(reDigit.FindString(s))
 }
 
+// DEPRECATED. use gstrconv package instead
 // ParseFloat32 parses a string into float32.
 func ParseFloat32(s string) (i float32) {
 	v, _ := strconv.ParseFloat(s, 32)
