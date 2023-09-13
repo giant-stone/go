ebe06a9702fa9b770439e9afec84aa0efeaa63b3
tony95271 <63030915+tony95271@users.noreply.github.com>
2023-09-13 12:20:11 +0800

    added grand.RandRange function #24

--- grand/int.go
+++ grand/int.go
@@ -10,3 +10,9 @@ func Rand(n int) int {
 	r1 := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return r1.Intn(n)
 }
+
+// Rand returns range in [min~max]
+func RandRange(min, max int) int {
+	r1 := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r1.Intn(max-min+1) + min
+}
