f78adccca159e8cf4791d722c6d203df96e24d4b
tony95271 <63030915+tony95271@users.noreply.github.com>
2023-09-13 12:20:11 +0800

    added grand.RandRange function #24

--- grand/int_test.go
+++ grand/int_test.go
@@ -2,6 +2,7 @@ package grand_test
 
 import (
 	"fmt"
+	"testing"
 
 	"github.com/giant-stone/go/grand"
 )
@@ -9,4 +10,38 @@ import (
 func ExampleRand() {
 	fmt.Println(grand.Rand(10))
 	fmt.Println(grand.Rand(3))
+}
+
+func ExampleRandRange() {
+	fmt.Println(grand.RandRange(1_000, 9_999))
+	fmt.Println(grand.RandRange(100_0000, 999_9999))
+}
+
+func TestRandRange(t *testing.T) {
+	type args struct {
+		min int
+		max int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "1_000~9_999",
+			args: args{
+				min: 1_000,
+				max: 9_000,
+			},
+			want: 123,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := grand.RandRange(tt.args.min, tt.args.max); got < tt.args.min || got > tt.args.max {
+				t.Errorf("RandRange() = %v, want %v < got < %v", got, tt.args.min, tt.args.max)
+			}
+		})
+	}
 }
